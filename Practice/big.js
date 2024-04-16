d4f5a9696a84b10f1acda8e22d5fe912a72557b8
Gaurav Juneja <junejagaurav0786@gmail.com>
2024-04-16 20:04:41 +0530

    html,css,js

---
+++ Practice/big.js
@@ -0,0 +1,14 @@
+function big(a,b)
+{
+    console.log("Value of A is: "+a);
+    console.log("Value of B is: "+b);
+    if(a>b)
+    {
+        console.log("Biggest Number is: "+a);
+    }
+    else
+    {
+        console.log("Biggest Number is: "+b);
+    }
+}
+big(20,30);
